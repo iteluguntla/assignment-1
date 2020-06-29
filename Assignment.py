8a435d17aa0d65ba804e6c8fea129e987f579af0
iteluguntla <67595141+iteluguntla@users.noreply.github.com>
2020-06-29 10:23:27 -0500

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,24 @@
+"""num = int(input("Enter a number: "))
+if(num % 2) == 0:
+   print("{0} is Even Number".format(num))
+else:
+   print("{0} is Odd Number".format(num))"""
+
+
+"""num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+if (num1 > num2) and (num1 > num3):
+   largest = num1
+elif (num1 < num2) and (num3 < num2):
+   largest = num2
+else:
+   largest = num3
+print("The largest number is",largest)"""
+
+
+"""number = int(input(" Please enter any integer : "))
+if((number % 5 == 0)):
+    print("Given number {0} is divisible by 5".format(number))
+else:
+    print("Given number {0} is not divisible by 5".format(number))"""
