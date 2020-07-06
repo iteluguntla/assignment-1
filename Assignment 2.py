905df45307af73cb02a43e57de8def6cc46613f3
iteluguntla <67595141+iteluguntla@users.noreply.github.com>
2020-07-05 19:41:39 -0500

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,35 @@
+#code for triangle
+"""print("enter the length of the triangles sides: ")
+x = int(input("side 1: "))
+y = int(input("side 2: "))
+z = int(input("side 3: "))
+if x == y == z:
+   print("Your trianlge is an equilateral tiangle") #all sides equal
+elif x == y or y == z or z == x:
+    print("Your trianlge is an isosceles tiangle")  #2 sides equal
+else:
+    print("Your trianlge is an scalene tiangle")    #no sides equal"""
+
+#code for number of days in month
+"""month = input("Enter the name of a month: ")
+
+if month == ("February" or "2"):
+    print("this month has 28/29 days")
+    
+elif month ("April" or "June" or "September" or "November" or "4" or "6" or "9" or "11"):
+    print("this month has 30 days")
+    
+elif month ("January" or "March" or "May" or "July" or "August" or "October" or "December" or "1" or "3" or "5" or "7" or "8" or "10" or "12"):
+    print("this month has 30 days")
+    
+else:
+    print("The month you have entered is invalid")"""
+
+#code to find given character is alphabet, number or special character
+"""ch = input("Enter any alphabet, number or special character:")
+if((ch>='a' and ch <='z') or (ch >= 'A' and ch<= 'Z')):
+    print("The given character ", ch, "is an alphabet")
+elif(ch >= '0' and ch <= '9'):
+    print("The given character ", ch, "is a number")
+else:
+    print("The given character ", ch, "is a special character")"""
